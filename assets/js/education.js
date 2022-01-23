346b6916417eb8c626ad457dacc8ac02c4bd33e7
Juan_Maniglia <jamr1989@gmail.com>
2022-01-23 18:02:22 +0100

    porfolio

---
+++ assets/js/education.js
@@ -0,0 +1,281 @@
+AOS.init();
+
+// MOOCs Cards
+
+const moocs = document.querySelector(".moocs");
+const moocscards = [
+  {
+    title: "I am Data Scientist",
+    cardImage: "assets\images\education-page\datasc.png",
+    moocLink: "https://api.badgr.io/public/assertions/8bLUrJhgSMyWKF-I7xOosQ?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Exploratory Data Analysis (EDA)",
+    cardImage: "assets\images\education-page\eda.png",
+    moocLink: "https://api.badgr.io/public/assertions/CzJV6Kp6SbCV1t1TepAJ-A?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Machine Learning",
+    cardImage: "assets\images\education-page\ml.png",
+    moocLink: "https://api.badgr.io/public/assertions/oYT8cQGQR6-_KWr0VOANCQ?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Python Fundamentals",
+    cardImage: "assets\images\education-page\python.png",
+    moocLink: "https://api.badgr.io/public/assertions/N4-Vzm1iSw2OBcwXVatcmA?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Data Analyst",
+    cardImage: "assets\images\education-page\data_ana.png",
+    moocLink: "https://api.badgr.io/public/assertions/DdXRakN0Q5GrXC8iNNGLsQ?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Deep Learning",
+    cardImage: "assets\images\education-page\deep.png",
+    moocLink: "https://api.badgr.io/public/assertions/W8PFdyotRSC95rZmYcR2Ag?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Machine Learning Engineer",
+    cardImage: "assets\images\education-page\enginee.png",
+    moocLink: "https://api.badgr.io/public/assertions/TeA-uJkiQ2q3Hzib0xIb3Q?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "NLP",
+    cardImage: "assets\images\education-page\nlp.png",
+    moocLink: "https://api.badgr.io/public/assertions/UZnYyzKbQfGWRtrCDUWc4Q?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "TensorFlow & Keras",
+    cardImage: "assets\images\education-page\tensor.png",
+    moocLink: "https://api.badgr.io/public/assertions/0er2sNKkSFWAmgSEsvyvhQ?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Time Series Analysis",
+    cardImage: "assets\images\education-page\time.png",
+    moocLink: "https://api.badgr.io/public/assertions/WsPl5iilQJeyusXv2SNLhA?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Data Wrangling",
+    cardImage: "assets\images\education-page\war.png",
+    moocLink: "https://api.badgr.io/public/assertions/07W2cTOZSQ6blC1674AOkw?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Data Visualization",
+    cardImage: "assets\images\education-page\visu.png",
+    moocLink: "https://api.badgr.io/public/assertions/0R4Qz9dgRNmlT9SDZl1cHg?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "MLOps",
+    cardImage: "assets\images\education-page\mlop.png",
+    moocLink: "https://api.badgr.io/public/assertions/aPD7R759S8SC2duhFKt4Tw?identity__email=jamr1989%40gmail.com",
+  },
+];
+
+const experience = [
+  {
+    img: "assets/images/education-page/c1.png"
+  },
+  {
+    img: "assets/images/education-page/c2.jpg"
+  },
+  {
+    img: "assets/images/education-page/c3.png"
+  },
+  {
+    img: "assets/images/education-page/c4.png"
+  },
+  {
+    img: "assets/images/education-page/c5.jpg"
+  },
+];
+
+let currentItem = 0;
+
+const img = document.getElementById('image');
+
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+window.addEventListener('DOMContentLoaded', function () {
+  showExperience();
+})
+
+function showExperience() {
+  setInterval(function () {
+    if (currentItem === experience.length) {
+      currentItem = 0;
+    }
+      const item = experience[currentItem];
+      img.src = item.img;
+      currentItem++;
+
+    },
+    3000);
+}
+
+const showCards = () => {
+  let output = "";
+  moocscards.forEach(
+    ({ title, cardImage, moocLink }) =>
+    (output += `        
+        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600" >  
+            <div class="card mb-3 mx-auto">
+               <div class="content">
+                  <div class="content-overlay"></div>
+                    <img src=${cardImage} class="card-img-top content-image">     
+                  <div class="content-details fadeIn-bottom">
+                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
+                  </div>
+                </div>
+                <div class="card-body">
+                    <h6 class="mt-0 py-2 text-center font-weight-bold mooc-title" style="font-size:12px;">${title}</h6>
+                </div>
+            </div>
+        </div>        
+      `)
+  );
+  moocs.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
+/* Badges*/
+
+const bagdes = document.querySelector(".badges");
+const badgesection = [
+  {
+    title: "I am Data Scientist",
+    image: "assets\images\education-page\datasc.png",
+    description: "https://api.badgr.io/public/assertions/8bLUrJhgSMyWKF-I7xOosQ?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Exploratory Data Analysis (EDA)",
+    image: "assets\images\education-page\eda.png",
+    description: "https://api.badgr.io/public/assertions/CzJV6Kp6SbCV1t1TepAJ-A?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Machine Learning",
+    image: "assets\images\education-page\ml.png",
+    description: "https://api.badgr.io/public/assertions/oYT8cQGQR6-_KWr0VOANCQ?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Python Fundamentals",
+    image: "assets\images\education-page\python.png",
+    description: "https://api.badgr.io/public/assertions/N4-Vzm1iSw2OBcwXVatcmA?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Data Analyst",
+    image: "assets\images\education-page\data_ana.png",
+    description: "https://api.badgr.io/public/assertions/DdXRakN0Q5GrXC8iNNGLsQ?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Deep Learning",
+    image: "assets\images\education-page\deep.png",
+    description: "https://api.badgr.io/public/assertions/W8PFdyotRSC95rZmYcR2Ag?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Machine Learning Engineer",
+    image: "assets\images\education-page\enginee.png",
+    description: "https://api.badgr.io/public/assertions/TeA-uJkiQ2q3Hzib0xIb3Q?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "NLP",
+    image: "assets\images\education-page\nlp.png",
+    description: "https://api.badgr.io/public/assertions/UZnYyzKbQfGWRtrCDUWc4Q?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "TensorFlow & Keras",
+    image: "assets\images\education-page\tensor.png",
+    description: "https://api.badgr.io/public/assertions/0er2sNKkSFWAmgSEsvyvhQ?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Time Series Analysis",
+    image: "assets\images\education-page\time.png",
+    description: "https://api.badgr.io/public/assertions/WsPl5iilQJeyusXv2SNLhA?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Data Wrangling",
+    image: "assets\images\education-page\war.png",
+    description: "https://api.badgr.io/public/assertions/07W2cTOZSQ6blC1674AOkw?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "Data Visualization",
+    image: "assets\images\education-page\visu.png",
+    description: "https://api.badgr.io/public/assertions/0R4Qz9dgRNmlT9SDZl1cHg?identity__email=jamr1989%40gmail.com",
+  },
+  {
+    title: "MLOps",
+    image: "assets\images\education-page\mlop.png",
+    description: "https://api.badgr.io/public/assertions/aPD7R759S8SC2duhFKt4Tw?identity__email=jamr1989%40gmail.com",
+  },
+];
+
+const showCards1 = () => {
+  let output = "";
+  badgesection.forEach(
+    ({ title, image, description }) =>
+    (output += `       
+      <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600"> 
+        <img class="img-fluid d-block mb-3 mx-auto hvr-grow" src="${image}" alt="Card image cap" width="200">
+          <div class="text-center font-weight-bolder" style="font-size: 1.3em;">${title}</div>
+          <div class="text-center text-muted font-weight-bolder p-2">${description}</div>
+      </div>`)
+  );
+  bagdes.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards1);
+
+
+/* Timeline Section*/
+
+$(function(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+    }
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  } else {
+    
+    sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  }
+  
+  sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+
+});
---
+++ assets/js/project.js
@@ -0,0 +1,159 @@
+AOS.init();
+/* Project Cards */
+
+const projectcards = document.querySelector(".projectcards");
+
+// Array of object for projects
+const projects = [
+  {
+    title: "Exploratory Data Analysis",
+    cardImage: "assets\images\project-page\Etapas-del-analisis-de-datos.png",
+    description: "Analisis exploratorio de datos sobre MAL.",
+    tagimg: "assets\images\project-page\python.svg",
+    Previewlink: "",
+    Githublink: "https://github.com/JuanManiglia/Exploratory_Data_Analysis",
+  },
+  {
+    title: "Machine Learning Project",
+    cardImage: "assets\images\project-page\Machine-Learning-01.jpg",
+    description: "Proyecto de ML (GANs, Sistema de recomendacion, Predictor de Score) MAL.",
+    tagimg: "assets\images\project-page\python.svg",
+    Previewlink: "",
+    Githublink: "https://github.com/JuanManiglia/Machine_Learning_Project",
+  },
+  {
+    title: "S&P 500",
+    cardImage: "assets\images\project-page\sp500.jpg",
+    description: "Analisis Exploratorio de Datos sobre el S&P500",
+    tagimg: "assets\images\project-page\python.svg",
+    Previewlink: "",
+    Githublink: "https://github.com/JuanManiglia/S-P500",
+  },
+  {
+    title: "SisRec",
+    cardImage: "assets\images\project-page\sysrec.jpg",
+    description: "Sistema de recomendación de Películas.",
+    tagimg: "assets\images\project-page\python.svg",
+    Previewlink: "",
+    Githublink: "https://github.com/JuanManiglia/ReSys_Movies",
+  },
+  {
+    title: "Recipe Ingredient Parser",
+    cardImage: "assets/images/project-page/recipe.jpg",
+    description: "Recipe Ingredient Parser built using JavaScript.",
+    tagimg: "assets\images\project-page\python.svg",
+    Previewlink: "",
+    Githublink: "",
+  },
+  {
+    title: "Bitcoin",
+    cardImage: "assets\images\project-page\bitcoin1.jpg",
+    description: "TimeSeries predictor de Bitcoin.",
+    tagimg: "assets\images\project-page\python.svg",
+    Previewlink: "",
+    Githublink: "https://github.com/JuanManiglia/Bitcoin",
+  },
+  {
+    title: "ML Mario",
+    cardImage: "assets\images\project-page\mario.png",
+    description: "ML de Mario por refuerzo.",
+    tagimg: "assets\images\project-page\python.svg",
+    Previewlink: "",
+    Githublink: "https://github.com/JuanManiglia/Mario_ML",
+  },
+  // {
+  //   title: "Personal Book Library",
+  //   cardImage: "assets/images/project-page/book.png",
+  //   description: "Personal book library app built using JavaScript.",
+  //   tagimg: "assets\images\project-page\python.svg",
+  //   Previewlink: "",
+  //   Githublink: "",
+  // },
+  // {
+  //   title: "Minesweeper Game",
+  //   cardImage: "assets/images/project-page/minesweeper.jpg",
+  //   description:
+  //     "Minesweeper Game built using python Object Oriented Programming.",
+  //   tagimg: "assets\images\project-page\python.svg",
+  //   Previewlink: "",
+  //   Githublink: "",
+  // },
+  // {
+  //   title: "Battery Life Calculator",
+  //   cardImage: "assets/images/project-page/battery.png",
+  //   description: "Built battery life calculator using Python.",
+  //   tagimg: "assets\images\project-page\python.svg",
+  //   Previewlink: "",
+  //   Githublink: "",
+  // },
+  // {
+  //   title: "Movie Recommendation System",
+  //   cardImage: "assets/images/project-page/movie-recommendation.jpeg",
+  //   description: "Built using Tensorflow.",
+  //   tagimg: "assets\images\project-page\python.svg",
+  //   Previewlink: "",
+  //   Githublink: "",
+  // },
+  // {
+  //   title: "Music Recommendation Engine",
+  //   cardImage: "assets/images/project-page/music.jpg",
+  //   description: "Built using Python.",
+  //   tagimg: "assets\images\project-page\python.svg",
+  //   Previewlink: "",
+  //   Githublink: "",
+  // },
+];
+
+// function for rendering project cards data
+const showCards = () => {
+  let output = "";
+  projects.forEach(
+    ({ title, cardImage, tags, Previewlink, Githublink }) => {
+      (output += `       
+        <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
+          <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
+            <div class="header">
+            </div>
+            <div class="data">
+              <div class="content">
+              <div class="title-div">
+                <h1 class="title"><a href="#">${title}</a></h1>
+                </div>
+            <ul class="menu-content"><br>
+                  <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
+                  <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>`
+      )
+    }
+  );
+  projectcards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+function myFunction() {
+  // Declare variables
+  var input, button, i, skillcard, card, title;
+  input = document.getElementById("myInput").value;
+  input = input.toUpperCase();
+  skillcard = document.getElementsByClassName("skill-card");
+  card = document.getElementsByClassName("card");
+  title = document.getElementsByClassName("title");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < button.length; i++) {
+    if (
+      button[i].innerHTML.toUpperCase().includes(input) ||
+      title[i].innerHTML.toUpperCase().includes(input)
+    ) {
+      skillcard[i].style.display = "";
+      card[i].style.display = "";
+    } else {
+      skillcard[i].style.display = "none";
+      card[i].style.display = "none";
+    }
+  }
+}
